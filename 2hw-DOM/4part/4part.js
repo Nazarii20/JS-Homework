abfbc481d0b31dd45d940b05419e76c4dea4f360
Nazarii20 <108357753+Nazarii20@users.noreply.github.com>
2022-09-18 23:04:20 +0300

    Add files via upload

---
+++ 2hw-DOM/4part/4part.js
@@ -0,0 +1,29 @@
+var red = document.querySelector('.red');
+red.addEventListener('click', ()=>{
+    red.style.color = 'red';
+});
+
+var yellow = document.querySelector('.yellow');
+yellow.addEventListener('click', ()=>{
+    yellow.style.color = 'yellow';
+});
+
+var green = document.querySelector('.green');
+green.addEventListener('click', ()=>{
+    green.style.color = 'green';
+});
+
+var blue = document.querySelector('.blue');
+blue.addEventListener('click', ()=>{
+    blue.style.color = 'blue';
+});
+
+var orange = document.querySelector('.orange');
+orange.addEventListener('click', ()=>{
+    orange.style.color = 'orange';
+});
+
+var gray = document.querySelector('.gray');
+gray.addEventListener('click', ()=>{
+    gray.style.color = 'gray';
+});
